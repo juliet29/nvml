ac2152816631628cece72218add25ec07763f2bf
juliet29 <jnwagwu@stanford.edu>
2026-06-24 17:53:43 -0700

    set up make flow graphs

--- src/nvml/cli/make/config.py
+++ src/nvml/cli/make/config.py
@@ -3,25 +3,34 @@ from typing import NamedTuple
 
 from plyze import CaseData
 
-
-class Paths:
-    data_store = Path()
-    pass
+PATH_TO_STORAGE = Path("/oak/stanford/groups/risheej/jnwagwu/storage/")
+PATH_TO_PROJECT_STORAGE = PATH_TO_STORAGE / "nvml"
 
 
-class Config(NamedTuple):
+class MakeConfig(NamedTuple):
     eplus_model_source: Path
-    data_store: Path
-    json_file_name: str
-    data_folder_name: str
     case_names: list[str]
-    idf_file_name: str
-    sql_file_name: str
+    data_store: Path
+    json_file_name: str = "out.json"
+    data_folder_name: str = "data"
+    idf_file_name: str = "out.idf"
+    sql_file_name: str = "eplusout.sql"
+    cardinal_expansion_factor = 1.1
 
-    def create_case_datas(self):
-        def make(case_name):
-            idf_path = self.eplus_model_source / case_name / self.idf_file_name
-            sql_path = self.eplus_model_source / case_name / self.sql_file_name
-            return CaseData(idf_path, sql_path)
+    def make_case_data(self, case_name):
+        idf_path = self.eplus_model_source / case_name / self.idf_file_name
+        sql_path = self.eplus_model_source / case_name / self.sql_file_name
+        return CaseData(idf_path, sql_path)
 
-        return [make(i) for i in self.case_names]
+    def make_json_path(self, case_name: str):
+        return self.data_store / case_name / self.data_folder_name / self.json_file_name
+
+    # def make_graph_reqs(self):
+    #     return []
+
+
+jun24config = MakeConfig(
+    eplus_model_source=PATH_TO_STORAGE / "msherlock_v1/data",
+    case_names=["100268", "100275", "100306"],
+    data_store=PATH_TO_PROJECT_STORAGE / "jun24",
+)
--- src/nvml/cli/make/create.py
+++ src/nvml/cli/make/create.py
@@ -1,20 +1,27 @@
+from pathlib import Path
+
 from cyclopts import App
+from loguru import logger
 from plyze import CaseData, FlowGraphModel
 from plyze.flow_graph.create.main import make_flow_graph
+
+from nvml.cli.make.config import MakeConfig
 
 create = App("create")
 
 
-def make_flow_graphs():
-    idf_sql_path = []
-    json_paths = []
-    data_folder_name = ""
-
+def make_flow_graphs(config: MakeConfig):
     def make_one_graph(case_data: CaseData, json_file_path: Path):
-        case_data = CaseData()
-        G = make_flow_graph()
-        FlowGraphModel.write(
-            G,
-        )
+        G = make_flow_graph(case_data, config.cardinal_expansion_factor)
+        FlowGraphModel.write(G, json_file_path, config.data_folder_name)
 
-    pass
+    failures = []
+    for case in config.case_names:
+        try:
+            cd = config.make_case_data(case)
+            jsf = config.make_json_path(case)
+            make_one_graph(cd, jsf)
+        except Exception as e:
+            logger.error(f"Failed to make {case} because: {e}")
+            failures.append({case: f"{e}"})
+        logger.success(f"Finished making {case}")
