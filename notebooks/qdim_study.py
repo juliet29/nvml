393a5600c421215a1e114d34b6596424e8f52423
juliet29 <jnwagwu@stanford.edu>
2026-07-03 13:18:01 -0700

    qdim wind direction vs sector compare

--- notebooks/qdim_study.py
+++ notebooks/qdim_study.py
@@ -7,173 +7,114 @@ app = marimo.App()
 @app.cell
 def _():
     import marimo as mo
-    from nvml.cli.studies.qdim import load_config, get_data_for_graph
+    from nvml.cli.studies.qdim import load_config, get_data_for_graph, get_all_data
     from nvml.cli.config import CONFIGS_DICT
     import altair as alt
+    import seaborn.objects as so
+    import seaborn as sns
 
     import polars as pl
     from nvml.constants import DataNames as dn
+    from nvml.utils import xr_to_polars
 
-    return CONFIGS_DICT, alt, dn, get_data_for_graph, pl
+    return dn, get_all_data, pl, so
 
 
 @app.cell
-def _(CONFIGS_DICT):
-    CONFIGS_DICT.keys()
+def _(get_all_data):
+    G, qoi, amb = get_all_data()
+    return amb, qoi
+
+
+@app.cell
+def _(qoi):
+    qoi
     return
 
 
 @app.cell
-def _(CONFIGS_DICT):
-    cfg = CONFIGS_DICT["case50"]
-    cfg
-    return (cfg,)
+def _(amb):
+    amb
+    return
+
+
+@app.cell
+def _(amb, dn):
+    ws = amb[dn.wind_sector]
+    ws
+    return
+
+
+@app.cell
+def _(amb, dn):
+    qoi_coords = {
+        dn.wind_sector : (dn.datetime, amb[dn.wind_sector].data),
+        dn.wind_dir : (dn.datetime, amb[dn.wind_dir].data),
+    }
+    return (qoi_coords,)
+
+
+@app.cell
+def _(dn, qoi, qoi_coords):
+    qa = qoi.expand_dims([dn.wind_dir, dn.wind_sector]).assign_coords(qoi_coords)
+    qa
+    return (qa,)
 
 
 @app.cell
 def _():
-    import io
-    import sys
-    from contextlib import contextmanager
-
-    @contextmanager
-    def suppress_stdout():
-        old_stdout = sys.stdout
-        sys.stdout = io.StringIO()
-        try:
-            yield
-        finally:
-            sys.stdout = old_stdout
-    # usage
-    # with suppress_stdout():
-    #     for ii in range(10):
-    #         print(ii)
-    return (suppress_stdout,)
-
-
-@app.cell
-def _(cfg):
-    case_name = cfg.get_one_case()
-    return (case_name,)
-
-
-@app.cell
-def _(case_name, cfg, get_data_for_graph):
-    df = get_data_for_graph(cfg, case_name)
     return
 
 
 @app.cell
-def _(get_data_for_graph, suppress_stdout):
-    def get_data(cfg, stride:int = 5):
-        cnt = 0
-        dfs = []
-        for i in cfg.case_names[0:-1:stride]:
-            print(f"{cnt}| Running case {i}")
-            cnt+=1
-            try: 
-                with suppress_stdout():
-                    r = get_data_for_graph(cfg, i);
-            except Exception as e: 
-                print([i, e])
-                continue
-            dfs.append(r)
-        return dfs 
-
-    return (get_data,)
+def _(dn, pl, qa):
+    df = pl.from_pandas(qa.reset_index(dims_or_levels=[dn.datetime, dn.space_name]).to_dataframe())
+    df
+    return (df,)
 
 
 @app.cell
-def _(cfg, get_data):
-    dfs2 = get_data(cfg)
-    return (dfs2,)
-
-
-@app.cell
-def _(dfs2, dn, pl):
-    tdf = dfs2[5].filter(pl.col(dn.wind_sector) == "E")
-    tdf
-    return (tdf,)
-
-
-@app.cell
-def _(alt, dn, tdf):
-    tdf.plot.scatter(x=alt.X(dn.wind_dir).scale(zero=False), y=dn.zone_dimless_flow, color=alt.Color(dn.incident_factor, type="ordinal").scale(scheme="lightgreyred"))
+def _(df, dn, so):
+    so.Plot(df, dn.wind_sector).add(so.Bars(), so.Hist()).show()
     return
 
 
 @app.cell
-def _(dn, tdf):
-    tdf.plot.bar(x=dn.wind_dir, y="count()")
+def _(df, dn, pl):
+    nedf = df.filter(pl.col(dn.wind_sector) == "N")
+    nedf
+    return (nedf,)
+
+
+@app.cell
+def _(dn, nedf, so):
+    so.Plot(nedf, dn.zone_dimless_flow).add(so.Area(), so.Hist(), color=dn.space_name).show()
     return
 
 
 @app.cell
-def _(dn, pl, tdf):
-    tdff = tdf.filter(pl.col(dn.wind_dir) == 90).group_by(dn.datetime).head(5)
-    tdff 
-    #.plot.bar(x=dn.zone_dimless_flow, y="count()", color=dn.space_name)
-    return (tdff,)
-
-
-@app.cell
-def _(alt, dn, tdf):
-    alt.Chart(tdf).transform_density(
-        density=dn.zone_dimless_flow,          # Column to calculate density for
-        groupby=[dn.space_name],
-        as_=[dn.zone_dimless_flow, 'density']  # Name output columns (X and Y axes)
-    ).mark_area(
-        opacity=0.4,
-        #color='steelblue'
-    ).encode(
-        x=dn.zone_dimless_flow,
-        y='density:Q',
-        color=dn.space_name
-    )
+def _(dn, nedf, so):
+    # lets look at the dist of wind directions.. 
+    so.Plot(nedf, dn.wind_dir).add(so.Bars(), so.Hist(bins=80)).show()
     return
 
 
 @app.cell
-def _(alt, dn, tdff):
-
-    alt.Chart(tdff).transform_density(
-        density=dn.zone_dimless_flow,
-        groupby=[dn.space_name],
-    ).mark_area().encode(
-        alt.X(dn.zone_dimless_flow),
-        alt.Y('density:Q', stack='zero'),
-        alt.Color(dn.space_name))
+def _(dn, nedf, pl):
+    # a table gives more of the info that is needed.. # TODO: put in utils
+    nedf.select(pl.col(dn.wind_dir).value_counts()).unnest().sort(by="count", descending=True)
     return
 
 
 @app.cell
-def _(dn, tdf):
-
-    tdf.plot.bar(x=dn.zone_dimless_flow, y="count()", color=dn.space_name, column=dn.wind_speed)
-    return
-
-
-@app.cell
-def _(dn, tdff):
-    tdff.plot.bar(x=dn.zone_dimless_flow, y="count()", color=dn.space_name, column=dn.datetime)
-    return
+def _(dn, nedf, pl):
+    nedf_wd = nedf.filter(pl.col(dn.wind_dir).is_in([342.5, 22.5]))
+    return (nedf_wd,)
 
 
 @app.cell
-def _(dn, pl, tdf):
-    tdf.filter(pl.col(dn.wind_dir) == 90).select(pl.col(dn.datetime).unique())
-    return
-
-
-@app.cell
-def _(dn, pl, tdf):
-    tdf.select(pl.col(dn.wind_dir).value_counts())
-    return
-
-
-@app.cell
-def _():
-    # spatial dist for given wind directions
+def _(dn, nedf_wd, so):
+    p = so.Plot(nedf_wd, dn.zone_dimless_flow).facet(row=dn.wind_dir).add(so.Area(), so.Hist(), color=dn.space_name).show()
     return
 
 
--- src/nvml/cli/studies/qdim.py
+++ src/nvml/cli/studies/qdim.py
@@ -4,14 +4,7 @@ from plyze import FlowGraphModel
 
 from nvml.cli.config import CONFIGS_DICT, MakeConfig
 from nvml.cli.studies.paths import ProjectPaths
-from nvml.constants import DataNames
 from nvml.io import get_ambient_data_as_ds, graph_to_ds
-from nvml.qdim.incident import (
-    calculate_incidence_factor,
-    make_zone_outward_normal_da,
-    wind_angle_da_to_vectors,
-)
-from nvml.qdim.intext import get_normals_for_windows_across_zones
 from nvml.qdim.wind import (
     add_incidence_data,
     add_wind_sector_coord,
@@ -20,35 +13,37 @@ from nvml.qdim.wind import (
 
 qdim = App("qdim")
 
-
 cfg = CONFIGS_DICT["jun24"]
+CASE_IX = 0
+CASE_NAME = cfg.get_one_case(CASE_IX)
 
 
 @qdim.command()
-def fb():
-    path = cfg.make_json_path(cfg.get_one_case())
+def get_graph(case_name: str = CASE_NAME):
+    path = cfg.make_graph_path(case_name)
     G = FlowGraphModel.read(path)
     return G
-    # return make_int_ext_series(G)
-    # sn = res.select("space_names").unique()
-    # dt = res.select("datetimes").unique()
-    # return res
 
 
 @qdim.command()
-def fc():
-    path = cfg.make_json_path(cfg.get_one_case())
+def get_qoi_ds(case_name: str = CASE_NAME):
+    path = cfg.make_graph_path(case_name)
     res = graph_to_ds(path)
-    # sn = res.select("space_names").unique()
-    # dt = res.select("datetimes").unique()
     return res
 
 
 @qdim.command()
-def fd():
-    res = get_ambient_data_as_ds(cfg.get_one_case_data().sql)
+def get_ambient_ds(ix: int = CASE_IX):
+    res = get_ambient_data_as_ds(cfg.get_one_case_data(ix).sql)
     res = add_wind_sector_coord(res)
     return res
+
+
+def get_all_data():
+    G = get_graph()
+    qoi_ds = get_qoi_ds()
+    ambient_ds = get_ambient_ds()
+    return G, qoi_ds, ambient_ds
 
 
 @qdim.command()
@@ -64,46 +59,6 @@ def fe():
     G = FlowGraphModel.read(json_path)
     df = prep_comparison_data(G, ambient_ds, qoi_ds)
     return df
-
-
-@qdim.command()
-def ff():
-    case_name = cfg.get_one_case(0)
-    graph_path = cfg.make_json_path(case_name)
-    idf_path = cfg.make_case_data(case_name).idf
-
-    case = EZ(idf_path)
-    G = FlowGraphModel.read(graph_path)
-    zons = get_normals_for_windows_across_zones(G, case)
-    zone_da = make_zone_outward_normal_da(zons)
-
-    ds = get_ambient_data_as_ds(cfg.get_one_case_data().sql)
-    wind_da = wind_angle_da_to_vectors(ds[DataNames.wind_dir])
-    incidence_factor = calculate_incidence_factor(zone_da, wind_da)
-    return incidence_factor
-
-
-@qdim.command()
-def ffa():
-    ds = get_ambient_data_as_ds(cfg.get_one_case_data().sql)
-    return wind_angle_da_to_vectors(ds[DataNames.wind_dir])
-
-
-@qdim.command()
-def ffb():
-    case_name = cfg.get_one_case(0)
-    graph_path = cfg.make_json_path(case_name)
-    idf_path, sql_path = cfg.make_case_data(case_name)  # .idf
-    case = EZ(idf_path)
-    G = FlowGraphModel.read(graph_path)
-
-    res = get_ambient_data_as_ds(sql_path)
-    ambient_ds = add_wind_sector_coord(res)
-
-    qoi_ds = graph_to_ds(graph_path)
-
-    cd = prep_comparison_data(G, ambient_ds, qoi_ds)
-    return add_incidence_data(G, case, ambient_ds, cd)
 
 
 def get_data_for_graph(cfg: MakeConfig, case_name: str):
